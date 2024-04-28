70cd0236917202a1e55bbee58a5b97fae3f00cb1
Monte Gardner <monteglenn@gmail.com>
2024-04-28 12:17:40 -0700

    capitalization
    capitalization fixes. The  production version of mysql is more case sensitive.

--- react-app/server/api/cart.js
+++ react-app/server/api/cart.js
@@ -20,7 +20,7 @@ const {verifyLoggedIn} = require('./verifyLoggedIn');
 
 router.use(verifyLoggedIn);
 router.get("/byLocation/:locationId", async function(req, res) {
-  const sql = "select c.id, quantity, inventoryId, price, p.name, I.locationId, L.name as locationName from cart C inner join inventory I on (C.inventoryId = I.id) inner join produce P on (I.produceId = P.id) inner join Location L on I.locationId = L.id where c.username=? and L.id=?";    
+  const sql = "select C.id, quantity, inventoryId, price, P.name, I.locationId, L.name as locationName from cart C inner join inventory I on (C.inventoryId = I.id) inner join produce P on (I.produceId = P.id) inner join location L on I.locationId = L.id where c.username=? and L.id=?";    
   const locationId = req.params.locationId;
   let results = await connection.promise().query(sql, [req.user, locationId]);
   results = results[0];
@@ -50,7 +50,7 @@ router.get('/allLocations', (req, res) => {
 
 
 
-    const sql = "select c.id, quantity, inventoryId, price, p.name, I.locationId, L.name as locationName from cart C inner join inventory I on (C.inventoryId = I.id) inner join produce P on (I.produceId = P.id) inner join Location L on I.locationId = L.id where c.username=?";
+    const sql = "select C.id, quantity, inventoryId, price, P.name, I.locationId, L.name as locationName from cart C inner join inventory I on (C.inventoryId = I.id) inner join produce P on (I.produceId = P.id) inner join Location L on I.locationId = L.id where C.username=?";
     console.log(sql);
     console.log(req.user);
     connection.query(sql, [req.user], (err,result) => {
@@ -135,7 +135,7 @@ router.get('/allLocations', (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  const sql = "select c.id, quantity, inventoryId, p.name, I.locationId from cart C inner join inventory I on (C.inventoryId = I.id) inner join produce P on (I.produceId = P.id) where c.username=?";
+  const sql = "select c.id, quantity, inventoryId, P.name, I.locationId from cart C inner join inventory I on (C.inventoryId = I.id) inner join produce P on (I.produceId = P.id) where C.username=?";
   console.log(sql);
   console.log(req.user);
   connection.query(sql, [req.user], (err,result) => {

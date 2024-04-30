5cae7f00a6c1a8cc0ea384e66013df1c362f9f11
Monte Gardner <monteglenn@gmail.com>
2024-04-30 10:12:21 -0700

    Purchase
    Redesgined info flow in purchase page . The cart and produce api have been combined to simplify error handling and info.

--- react-app/server/api/produce.js
+++ react-app/server/api/produce.js
@@ -12,8 +12,6 @@ const {verifyLoggedIn }= require('./verifyLoggedIn');
 router.use(verifyLoggedIn);
 
 router.get('/', async function(req, res) {  
-    const test = "select serialNumber, quantityAvailable, inventoryId from crate";
-    const testResults = await connection.promise().query(test);
 
     
     const sql = "select I.id, P.name, sum(C.quantityAvailable) as quantityAvailable, I.price from inventory I  inner join produce P  on I.produceId = P.id inner join crate C on (I.id=C.inventoryId) where I.locationId=? group by inventoryId";
@@ -28,6 +26,34 @@ router.get('/', async function(req, res) {
             res.json(result);
         }
     });
+});
+
+//returns all produce at the given location as well as the amount, if any, in the current users cart.
+router.get("/produceAndCart/:location", async function(req,res) {
+    var locationId = req.params.location;
+
+    const sql = "select I.id, P.name, sum(C.quantityAvailable) as quantityAvailable, I.price from inventory I  inner join produce P  on I.produceId = P.id inner join crate C on (I.id=C.inventoryId) where I.locationId=? group by inventoryId";
+    console.log(sql);
+    let produceResult = await connection.promise().query(sql, [locationId]);
+    produceResult = produceResult[0];
+    
+    const cartSql = "select C.id, C.inventoryId, C.quantity from cart C inner join inventory I on (C.inventoryId = I.id) where username=? and I.locationId=?";
+    let cartResult = await connection.promise().query(cartSql, [req.user, locationId]);
+    cartResult = cartResult[0];
+    
+    for (let index=0; index < produceResult.length; index++) {
+        let checkProduce = produceResult[index];
+        for (let c = 0; c < cartResult.length; c++) {
+            let checkCart = cartResult[c];
+            if (checkCart.inventoryId == checkProduce.id) {
+                checkProduce.quantity = checkCart.quantity;
+                checkProduce.cartId = checkCart.id;
+            }
+        }
+    }
+    res.status(200);
+    res.json(produceResult);
+    
 });
 
 module.exports = router;
--- react-app/src/purchase.js
+++ react-app/src/purchase.js
@@ -14,7 +14,6 @@ const Purchase = () => {
   const [locations, setLocations] = useState([]);
   const [products, setProducts] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
-  const [cart, setCart] = useState([]); 
   const [errors, setErrors] = useState([]);
   
   useEffect(() => {
@@ -31,7 +30,8 @@ const Purchase = () => {
 
   useEffect(() => {
     if (myLocation) {
-      axios.get("/api/produce?location=" + myLocation).then(function(response) {
+      
+      axios.get("/api/produce/produceAndCart/" + myLocation).then(function(response) {
           if (response.status == 200) {
             setProducts(response.data);
           } else {
@@ -42,35 +42,6 @@ const Purchase = () => {
 
   }, [myLocation]);
 
-
-  useEffect(() => {
-    //go get the cart and match it to products in this location
-    axios.get("/api/cart/").then(function(response) {
-       if (response.status == 200) {
-        var newCart  = [];
-        for (var index=0; index < response.data.length; index++) {
-          var cartItem = response.data[index];
-          if (cartItem.locationId == myLocation){
-            var cartRecord = {
-              id: cartItem.id,
-              inventoryId: cartItem.inventoryId,
-              quantity: cartItem.quantity
-            };
-            newCart.push(cartRecord);
-            var relatedInput = document.getElementById(cartItem.inventoryId);
-            if (relatedInput) {
-               relatedInput.setAttribute("value", cartItem.quantity);  //should just do this once.
-            }
-          }
-
-        }
-        setCart(newCart);
-        console.log(cart);
-       } else {
-        setErrorMessage(JSON.stringify(response));
-       }
-    });
-  }, [products])
 
 
   function findProduct(inventoryId) {
@@ -85,17 +56,18 @@ const Purchase = () => {
   function enforceMaxQuantity() {
 
     var isClean = true;
-    for (let index=0; index < cart.length; index++) {
-      let checkCart = cart[index];
-      let productIndex = findProduct(checkCart.inventoryId);
-      let checkProduct = products[productIndex];
-      let isError = parseInt(checkCart.quantity) > parseInt(checkProduct.quantityAvailable);
-      var newErrors = [];
-      if (isError) {
-        newErrors[productIndex] = "You can't buy more than " + checkProduct.quantityAvailable;
-        isClean = false;
-      } else {
-        newErrors[productIndex] = null;
+    var newErrors = [];
+    for (let index=0; index < products.length; index++) {
+      let checkProduct = products[index];            
+       if (checkProduct.quantity) {
+        let isError = parseInt(checkProduct.quantity) > parseInt(checkProduct.quantityAvailable);
+      
+        if (isError) {
+          newErrors[index] = "You can't buy more than " + checkProduct.quantityAvailable;
+          isClean = false;
+        } else {
+          newErrors[index] = null;
+        }
       }
     }
     setErrors(newErrors);
@@ -105,36 +77,43 @@ const Purchase = () => {
   //occurs on each change of quantity
   function updateQuantity(inventoryId, quantity) {
     console.log("Update " + inventoryId + "," + quantity);
-    var found = false;
-    for (let index=0; index < cart.length; index++) {
-      if (cart[index].inventoryId == inventoryId) {
-        cart[index].quantity = quantity;
-        found = true;
+    for (let index=0; index < products.length; index++) {
+      if (products[index].id == inventoryId) {
+        products[index].quantity = quantity;
+
       }
     }
-    if (!found) {
-      cart.push({
-        inventoryId: inventoryId,
-        quantity: quantity
-      });
-    }
-    enforceMaxQuantity();
-    
+    enforceMaxQuantity();    
   }  
 
   //occurs only when they click update cart
   function updateCart(e) {    
     e.preventDefault();
     let isClean = enforceMaxQuantity();
+    
     if (isClean) {
+      let cart = [];
+      for (let index=0; index < products.length; index++) {
+        let product = products[index];
+        if (product.quantity != null && parseInt(product.quantity) > 0) {
+          cart.push({
+            id: product.cartId,
+            quantity: product.quantity,
+            inventoryId: product.id
+          });
+        } else {
+          product.quantity = null;
+        }
+      }  
       axios.put("/api/cart/", cart).then(function(response) {
           alert("Cart Updated");  //TODO: Toast Library
       }).catch(function(err) {
         alert(err.response.data);
+        //loadCart();
+        setMyLocation(myLocation);
       });
     } else {
-      alert("You have cart errors.")
-      
+      alert("You have cart errors.")      
     }
     return true;
   }
@@ -168,9 +147,8 @@ const Purchase = () => {
                       <td>{product.quantityAvailable}</td>
                       <td>{product.price}</td>
                       <td>
-                        <input type="number" id = {product.id} min="0"
-                          onChange= {(e) => updateQuantity(product.id, e.target.value)}
-                          >
+                        <input type="number" id = {product.id} min="0" value = {product.quantity}
+                          onChange= {(e) => updateQuantity(product.id, e.target.value)}>
                         </input>
                       </td>
                      
